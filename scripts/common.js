0914002b628915d5853cdf8e385ac3a673ee3f04
K. Singh <16361374+yourskiss@users.noreply.github.com>
2023-01-14 01:35:10 +0530

    ui

---
+++ scripts/common.js
@@ -0,0 +1,23 @@
+/* page refresh on orientation change === START */
+$(window).on('orientationchange', function () 
+{
+    location.reload(true);
+});
+/* page refresh on orientation change === END */
+
+/* Load === start */
+$(window).on('load', function() 
+{
+     
+});
+/* Load === end */
+
+
+ 
+
+  
+
+ 
+
+
+ 
