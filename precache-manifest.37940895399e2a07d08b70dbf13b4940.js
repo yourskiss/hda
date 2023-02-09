499493e6ca4563f5784d3a7f8f9116fc839bad7f
K. Singh <16361374+yourskiss@users.noreply.github.com>
2023-02-09 23:36:06 +0530

    ui

---
+++ precache-manifest.37940895399e2a07d08b70dbf13b4940.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "183d2e31212e7916e4f4",
+    "url": "/static/css/main.d2685dde.chunk.css"
+  },
+  {
+    "revision": "183d2e31212e7916e4f4",
+    "url": "/static/js/main.183d2e31.chunk.js"
+  },
+  {
+    "revision": "9d1403bc48fad47d212b",
+    "url": "/static/js/1.9d1403bc.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "61b2c8850761aad0ecc9f148430a3d7a",
+    "url": "/static/media/about-hda.61b2c885.png"
+  },
+  {
+    "revision": "eb7da0ef2363f1169b7ffe5bb01bc15b",
+    "url": "/static/media/poster-join-the-community.eb7da0ef.png"
+  },
+  {
+    "revision": "c21f1a9dff6bb437894655f99b1cd3f3",
+    "url": "/static/media/poster-member-project.c21f1a9d.png"
+  },
+  {
+    "revision": "e6bbf55b670357f1bf56992fc90a786e",
+    "url": "/static/media/poster-development-resources.e6bbf55b.png"
+  },
+  {
+    "revision": "bd5cd42aaf58417f420d93b705bf30eb",
+    "url": "/static/media/building-value-together.bd5cd42a.png"
+  },
+  {
+    "revision": "8f87a3b1c6d48e2ea97ab3d6e119cc56",
+    "url": "/static/media/bg-top.8f87a3b1.png"
+  },
+  {
+    "revision": "4b1b88b1523744ea43a7d9be2fae2141",
+    "url": "/static/media/Gilroy-Regular.4b1b88b1.woff2"
+  },
+  {
+    "revision": "c7d7947ddc175c76c94f31acf8f61e4c",
+    "url": "/static/media/Gilroy-Regular.c7d7947d.woff"
+  },
+  {
+    "revision": "de0a7f5c6036ebd943f6590bdfd326ef",
+    "url": "/index.html"
+  }
+];
